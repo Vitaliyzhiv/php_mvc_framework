2b4632f89103440efd6147353e873ae44a2c3d3c
vitaliyzhiv <zhmud12@gmail.com>
2024-11-16 06:13:05 +0200

    push changes to main branch

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -38,7 +38,7 @@
 
 
 
-    <script src="<?= base_url('/assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+    <script src="<?= base_url('/assets/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 
 
 </body>
