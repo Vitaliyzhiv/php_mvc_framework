c9829df9abcaa66ffcc3bf85ca973cdbd781409a
vitaliyzhiv <zhmud12@gmail.com>
2024-11-16 05:40:14 +0200

    configurational file for constants etc

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("ROOT", dirname(__DIR__));
+
+// public folder
+const WWW = ROOT . '/public';
+const CONFIG = ROOT . '/config';
+const HELPERS = ROOT . '/helpers';
+const APP = ROOT . '/app';
+const CORE = ROOT . '/core';
+const VIEWS = APP . '/Views';
+const LAYOUT = 'default';
+const PATH = 'http://localhost';
