c07024d642246a986862adbf68d438b27bbc9ae2
vitaliyzhiv <zhmud12@gmail.com>
2024-11-16 06:00:22 +0200

    Set up application initialization and configuration
    - Check for PHP version requirement (PHP 8 or higher)
    - Include configuration file and autoloader
    - Initialize the application instance
    - Include helper functions for easier access
    - Load routes configuration and start the application

--- public/index.php
+++ public/index.php
@@ -7,5 +7,25 @@ if (PHP_MAJOR_VERSION < 8) {
    die("PHP 8 or higher is required");
 }
 
+// подключаем конфиг
+require_once __DIR__ . '/../config/config.php';
+// подключаем автозагрузчик классов
+require_once ROOT . '/vendor/autoload.php';
+require_once HELPERS . '/helpers.php';
+
+$app = new PHPFramework\Application();
+
+require_once CONFIG . '/routes.php';
+
+$app->run();
+
+
+// dump(app());
+// dump($app);
+// dump(request()->getMethod());
+// dump(request()->isGet());
+// dump(request()->isPost());
+// // проверяем параметр page в строке
+// dump(request()->get('page'));
 // время выполнения скрипта
-var_dump("Time: " . (microtime(true) - $start_framework));
+// dump("Time: " . (microtime(true) - $start_framework));
