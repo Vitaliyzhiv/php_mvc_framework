5a05a4c7cf451a90f582b50e247cb9d510b744dc
vitaliyzhiv <zhmud12@gmail.com>
2024-11-17 04:46:13 +0200

    added csrf token generation func

--- core/Application.php
+++ core/Application.php
@@ -23,10 +23,26 @@ class Application
         $this->router = new Router($this->request, $this->response);
         $this->view = new View(LAYOUT);
         $this->session = new Session();
+        $this->generateCsrfToken();
     }
 
     public function run(): void
     {
         echo $this->router->dispatch();
+    }
+
+    /**
+     * Generates a CSRF token and stores it in the session if it does not already exist.
+     * This token is used to protect against Cross-Site Request Forgery attacks.
+     */
+    public function generateCsrfToken() :void {
+        // Check if the session does not already have a CSRF token
+        if (!session()->has('csrf_token')) {
+            // Generate a new CSRF token using a secure random bytes generator
+            // Convert the bytes to a hexadecimal representation
+            $csrfToken = bin2hex(random_bytes(32));
+            // Store the generated token in the session under the key 'csrf_token'
+            session()->set('csrf_token', $csrfToken);
+        }
     }
 }
