0c81fefa7e5b425a19aea125c874d9f123ef2808
vitaliyzhiv <zhmud12@gmail.com>
2024-11-17 04:42:48 +0200

    view for security error

---
+++ app/Views/errors/419.php
@@ -0,0 +1,16 @@
+<!-- шаблон страницы 404 -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Security Error 419</title>
+ </head>
+ <body>
+    
+    <h1> Security Error 419</h1>
+    <p><?=$error ?? '' ?></p>
+
+    <a href="/">Вернуться на главную страницу</a>
+ </body>
+ </html>
