6ea911c4886aaea7a8cf3f9ec7f124a44e167c32
vitaliyzhiv <zhmud12@gmail.com>
2024-11-17 04:52:59 +0200

    Added CSRF token checks and withoutCsrfToken function for routes not requiring CSRF

--- core/Router.php
+++ core/Router.php
@@ -4,8 +4,8 @@ namespace PHPFramework;
 
 class Router
 {
-//    protected Request $request;
-//    protected Response $response;
+    //    protected Request $request;
+    //    protected Response $response;
 
     protected array $routes = [];
     protected array $route_params = [];
@@ -19,9 +19,7 @@ class Router
     public function __construct(
         protected Request $request,
         protected Response $response
-    )
-    {
-    }
+    ) {}
 
     public function add($path, $callback, $method): self
     {
@@ -37,7 +35,7 @@ class Router
             'callback' => $callback,
             'middleware' => null,
             'method' => $method,
-            'needToken' => true,
+            'needCsrfToken' => true,
         ];
         return $this;
     }
@@ -80,15 +78,80 @@ class Router
                 &&
                 in_array($this->request->getMethod(), $route['method'])
             ) {
-                foreach ($matches as $k => $v) {
-                    if (is_string($k)) {
-                        $this->route_params[$k] = $v;
+
+                // проверять наличие csrf токена имеет смысл только в POST запросе, так как важные данные методом GET не отправляют 
+
+                if (request()->isPost()) {
+                    // если запрос требует csrf токена и метод проверки токена не прошел, то возвращаем ошибку
+                    if ($route['needCsrfToken'] && !$this->checkCsrfToken()) { 
+
+                        // если данные были обработанны ajax запросом то возвращаем ошибку с помощью json_encode
+                        if (request()->isAjax()) {
+                            echo json_encode([
+                                'status' => 'error',
+                                'data' => 'Security error',
+                            ]);
+                            die;
+                        } else {
+                            // // устанавливаем flash сообщение об ошибке
+                            // session()->setFlash('error', 'Ошибка безопасности');
+                            // // делаем редирект на текущую страницу
+                            // response()->redirect();
+
+                            //  другой вариант делать abort 419 как в ларавель
+                            abort("Ошибка безопасности", 419);
+                        }
+
                     }
                 }
+                    foreach ($matches as $k => $v) {
+                        if (is_string($k)) {
+                            $this->route_params[$k] = $v;
+                        }
+                    }
                 return $route;
             }
         }
         return false;
+    }
+
+    /**
+     * This function allows to disable csrf token check for some routes.
+     * We use array_key_last() to get the last route added to the routes array
+     * and set the 'needCsrfToken' key to false.
+     * This way, the route will not be checked for csrf token.
+     *
+     * @return self
+     */
+    public function withoutCsrfToken():self {
+        $lastRouteKey = array_key_last($this->routes);
+        $this->routes[$lastRouteKey]['needCsrfToken'] = false;
+        return $this;
+    }
+
+    /**
+     * This function checks if the csrf token is present in the request and matches
+     * the token stored in the session.
+     *
+     * @return bool True if the csrf token is valid, false otherwise.
+     */
+    public function checkCsrfToken(): bool
+    {
+        // Check if the csrf token is present in the request
+        // as a post parameter
+        if (!request()->post('csrf_token')) {
+            return false;
+        }
+
+        // Check if the csrf token matches the token stored in the session
+        // If it does not match, return false
+        if (request()->post('csrf_token') !== session()->get('csrf_token')) {
+            return false;
+        }
+
+        // If we reach this point, that means the csrf token is valid
+        // so return true
+        return true;
     }
 
 }
