a390109c0a0182e5f0bfc4d2b7bf1878bc2919d2
vitaliyzhiv <zhmud12@outlook.com>
2024-11-19 03:17:21 +0200

    Refactor UserController: Save user data to database on successful registration
    - Replaced manual query creation with Eloquent's `save()` method to store user data.
    - Updated the store() method to include success and error messages based on the result of the registration.
    - Cleaned up commented-out code and added session flash messages for feedback.

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,23 +3,30 @@
 namespace App\Controllers;
 
 use App\Models\User;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
-class UserController extends BaseController {
+class UserController extends BaseController
+{
 
     // функция которая возвращает страницу регистрации
-    public function register() {
+    public function register()
+    {
+
+
+
+        // dump($users);
         // возвращаем вид с помощью функции helper view
         return view('user/register', [
             // передаем в вид данные для формы регистрации
-            'title' => 'Register page' 
+            'title' => 'Register page'
         ]);
-
     }
 
     // Функция которая обрабатывает данные формы регистрации
-    public function store() {
+    public function store()
+    {
         // создаем экземпляр класса User, который является моделью
-        $model = new User(); 
+        $model = new User();
         // вызываем метод loadData из класса User
         $model->loadData();
         // проверяем прошла ли успешно валидация формы
@@ -32,10 +39,24 @@ class UserController extends BaseController {
             session()->set('form_errors', $model->getErrors());
             // устанавливаем данные формы в сессию
             session()->set('form_data', $model->attributes);
-            
         } else {
-            // session()->setFlash('info', ('Info Message...'));
-            session()->setFlash('success', 'Successfully validation');
+
+            // сохраняем данные в БД с помощью метода create
+            // User::query()->create([
+            //     'name' => $model->attributes['name'],
+            //     'email' => $model->attributes['email'],
+            //     'password' => $model->attributes['password'],
+            // ]);
+            
+            // сохраняем данные с помошью метода save
+            // Проверяем статус сохранения данных
+            if ( $model->save() ) {
+                session()->setFlash('success', 'Thank you for registration');
+            } else {
+                session()->setFlash('error', 'Error Registration');
+            }
+
+
         }
 
         //  делаем редирект на нужную страницу
@@ -48,14 +69,12 @@ class UserController extends BaseController {
     }
 
     // Страница которая возвращает функцию логина
-    public function login() {
-         // возвращаем вид с помощью функции helper view
-         return view('user/login', [
+    public function login()
+    {
+        // возвращаем вид с помощью функции helper view
+        return view('user/login', [
             // передаем в вид данные для формы регистрации
-            'title' => 'Login page' 
+            'title' => 'Login page'
         ]);
     }
-
-
-
 }
