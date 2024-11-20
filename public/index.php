4b41324e13058f182aa47f6c9d5339b0bf96f9a0
vitaliyzhiv <zhmud12@outlook.com>
2024-11-20 12:23:25 +0200

    adding whoop object class from library Whoops for pretty error logging

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
+
+use Whoops\Handler\CallbackHandler;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
 // замеряем время выполнения скрипта
 $start_framework = microtime(true);
@@ -12,6 +16,28 @@ require_once __DIR__ . '/../config/config.php';
 // подключаем автозагрузчик классов
 require_once ROOT . '/vendor/autoload.php';
 require_once HELPERS . '/helpers.php';
+
+// создаем экземпляр класса Whoops для вывода ошибок
+$whoops = new Run();
+// проверяем статус константы DEBUG для установки нужного режима отладки
+if (DEBUG) {
+   // режим разработки, выводим ошибки на экран
+   $whoops->pushHandler(new PrettyPageHandler());
+} else {
+   // режим продакшн, выводим ошибки в лог
+   $whoops->pushHandler(new CallbackHandler(function (Throwable $e) {
+      // dump($e);
+      // в файл лога записываем название файла где произошла ошибка, строку и само сообщение об ошибке
+      error_log("File: {$e->getFile()}, Line: {$e->getLine()}, Message: {$e->getMessage()}\n", 3, ERROR_LOGS);
+      // вызываем функцию abort для перенаправления на страницу ошибки
+      abort("Some error", 500);
+      
+   }));
+}
+// запускаем Whoops
+$whoops->register();
+
+
 
 $app = new PHPFramework\Application();
 
