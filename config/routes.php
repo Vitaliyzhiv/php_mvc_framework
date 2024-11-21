8e6608446120df26fae842eb7736192fd6bfbbe0
vitaliyzhiv <zhmud12@outlook.com>
2024-11-21 06:46:26 +0200

    created const MIDDLEWARE for assigning keys from itself to specified classes which are the values of keys

--- config/routes.php
+++ config/routes.php
@@ -5,8 +5,15 @@
 use App\Controllers\HomeController;
 use App\Controllers\UserController;
 
+// создаем ассоциативный массив константу middleware для соотношения к какому классу относится конкретный 
+// ключ в $routes['middleware']
+const MIDDLEWARE = [
+    'auth' => \PHPFramework\Middleware\Auth::class
+];
+
 
 $app->router->get('/', [HomeController::class, 'index']);
+$app->router->get('/dashboard', [HomeController::class, 'dashboard'])->middleware(['auth']);
 $app->router->get('/register', [UserController::class, 'register']);
 // получение данных из формы при регистрации
 $app->router->post('/register', [UserController::class, 'store']);
@@ -14,4 +21,4 @@ $app->router->get('/login', [UserController::class, 'login']);
 
 
 
-//dump($app->router->getRoutes());
+// dump($app->router->getRoutes());
