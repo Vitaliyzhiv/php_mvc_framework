0823bb1b81343b43edaa00dca6edb7a8e834127f
vitaliyzhiv <zhmud12@outlook.com>
2024-11-23 12:50:24 +0200

    added pagination view for page

--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -2,6 +2,10 @@
     <!-- выводим пользователей циклом -->
     <?php foreach ($users as $user) : ?>
         <?= $user['name']; ?><br>
-        <? //= $user['email']; ?>
+        <? //= $user['email']; 
+        ?>
     <?php endforeach; ?>
+    <br>
+    <!-- выводим pagination  -->
+    <?= $pagination; ?>
 </div>
