86a1beaa6397a19ebd5e70b9e538321d796dce01
vitaliyzhiv <zhmud12@outlook.com>
2024-11-25 14:25:34 +0200

    remove class active as of now they will be automatically added using js (jquery) and add class navbar-menu for using it as anchor when work or modify menu with js

--- app/Views/incs/menu.php
+++ app/Views/incs/menu.php
@@ -1,9 +1,9 @@
-<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<ul class="navbar-nav me-auto mb-2 mb-lg-0 navbar-menu">
     <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="<?= base_url('/'); ?>">Home</a>
+        <a class="nav-link " aria-current="page" href="<?= base_url('/'); ?>">Home</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="<?= base_url('/dashboard'); ?>">Dashboard</a>
+        <a class="nav-link " aria-current="page" href="<?= base_url('/dashboard'); ?>">Dashboard</a>
     </li>
     <li class="nav-item">
         <a class="nav-link " aria-current="page" href="<?= base_url('/register'); ?>">Register</a>
