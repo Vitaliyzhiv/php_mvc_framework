5c9837a42fa658c5a0b64c49b733845974489c34
vitaliyzhiv <zhmud12@outlook.com>
2024-11-27 16:08:16 +0200

    refactor: - replace base_url func  to base_href - replace static phrases in forms and placeholders to specified language strings

--- app/Views/user/register.php
+++ app/Views/user/register.php
@@ -9,40 +9,52 @@
     <div class="row">
         <div class="col-md-6 offset-md-3">
 
-            <form action="<?= base_url('/register'); ?>" method="post" class="ajax-form">
+            <form action="<?= base_href('/register'); ?>" method="post" class="ajax-form">
 
                 <!-- вызываем функцию helper getCsrfField  для добавления поля csrf_token -->
                 <?= get_csrf_field(); ?>
                 <!-- name -->
                 <div class="mb-3">
-                    <label for="name" class="form-label">Name </label>
+                    <label for="name" class="form-label">
+                        <?php _e('user_register_name'); ?>
+                    </label>
                     <!-- с помощью функции хелпера old сохраняем заполненные значения -->
-                    <input type="text" name="name" class="form-control <?= get_validation_class('name'); ?>" value="<?= old('name'); ?>" id="name" placeholder="name">
+                    <input type="text" name="name" class="form-control <?= get_validation_class('name'); ?>" value="<?= old('name'); ?>" id="name"
+                        placeholder="<?php _e('user_register_name'); ?>">
                     <!-- выводим ошибку для поля с помощью helper get_errors -->
                     <?= get_errors('name'); ?>
                 </div>
                 <!-- email -->
                 <div class="mb-3">
-                    <label for="email" class="form-label">Email </label>
+                    <label for="email" class="form-label">
+                        <?php _e('user_register_email'); ?>
+                    </label>
                     <input type="email" name="email" value="<?= old('email'); ?>" class="form-control <?= get_validation_class('email'); ?>" id="email" placeholder="name@example.com">
                     <!-- выводим ошибку для поля с помощью helper get_errors -->
                     <?= get_errors('email'); ?>
                 </div>
                 <!-- password -->
                 <div class="mb-3">
-                    <label for="password" class="form-label">Password </label>
-                    <input type="password" name="password" class="form-control <?= get_validation_class('password'); ?>" id="password" placeholder="Password">
+                    <label for="password" class="form-label">
+                        <?php _e('user_register_password'); ?>
+                    </label>
+                    <input type="password" name="password" class="form-control <?= get_validation_class('password'); ?>" id="password"
+                        placeholder="<?php _e('user_register_password'); ?>">
                     <?= get_errors('password'); ?>
                 </div>
                 <!-- password confirm -->
                 <div class="mb-3">
-                    <label for="confirmPassword" class="form-label">Confirm password </label>
-                    <input type="password" name="confirmPassword" class="form-control <?= get_validation_class('confirmPassword'); ?>" id="confirmPassword" placeholder="Confirm password">
+                    <label for="confirmPassword" class="form-label">
+                        <?php _e('user_register_confirmPassword'); ?>
+                    </label>
+                    <input type="password" name="confirmPassword" class="form-control <?= get_validation_class('confirmPassword'); ?>" id="confirmPassword"
+                        placeholder="<?php _e('user_register_confirmPassword'); ?>">
                     <?= get_errors('confirmPassword'); ?>
                 </div>
                 <!-- submit button -->
-                <button type="submit" class="btn btn-warning">Register</button>
-
+                <button type="submit" class="btn btn-warning">
+                    <?php _e('user_register_button'); ?>
+                </button>
             </form>
 
             <!-- с помощью метода remove класса Session убираем вывод ошибок -->
