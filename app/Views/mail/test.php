b585eb54c15f0c3595ad6b151a0945df8e49834e
vitaliyzhiv <zhmud12@outlook.com>
2024-11-27 17:32:21 +0200

    created: - test mail template

---
+++ app/Views/mail/test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background: #eee;
+        }
+    </style>
+</head>
+<body>
+    
+    <h1>Тестовое письмо</h1>
+    <p>NAME: <?= $name ?? ''; ?></p>
+    <p>AGE: <?= $age ?? ''; ?></p>
+
+
+</body>
+</html>
