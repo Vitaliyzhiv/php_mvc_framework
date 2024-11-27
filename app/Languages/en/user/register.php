bf92c17f437be3d6a4a8ed7d8da943a980ec5946
vitaliyzhiv <zhmud12@outlook.com>
2024-11-27 11:54:59 +0200

    created: - files with array containing translated phrases for user register view

---
+++ app/Languages/en/user/register.php
@@ -0,0 +1,10 @@
+<?php 
+
+return [
+    'user_register_name' => 'Name',
+    'user_register_email' => 'Email',
+    'user_register_password' => 'Password',
+    'user_register_confirmPassword' => 'Confirm password',
+    'user_register_button' => 'Register',
+]; 
+
