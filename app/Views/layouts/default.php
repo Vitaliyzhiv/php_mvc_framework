4ae11ded396f56d44f9b016a61b0ae698d47de6a
vitaliyzhiv <zhmud12@outlook.com>
2024-11-27 11:59:11 +0200

    added: - meta teg base_href for correct loading resources

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+    <!-- Добавляем этот мета тег чтобы пути  к картинкам и так же к страницам пагинации формировались правильно -->
+    <base href="<?= base_url('/'); ?>">
     <meta charset="UTF-8">
     <!-- мета поле для csrf токена -->
     <?= get_csrf_meta(); ?>
@@ -28,13 +30,38 @@
 
 <nav class="navbar navbar-expand-lg bg-dark mb-3" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="<?= base_href(); ?>"><?php _e('tpl_logo')?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- выводим меню из кеша -->
             <?= cache()->get('menu'); ?>
+
+            <?php $request_uri = uri_without_lang();  ?>
+            <div class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php $current_lang = app()->get('lang')['title'] ?>
+                        <?= $current_lang; ?>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <?php foreach (LANGS as $key => $value): ?>
+                            <?php if ($value['title'] != $current_lang): ?>
+                                <?php if ($value['base'] == 1): ?>
+                                    <li>
+                                        <a class="dropdown-item" href="<?= base_url("{$request_uri}"); ?>"><?= $value['title']; ?></a>
+                                    </li>
+                                <?php else: ?>
+                                    <li>
+                                        <a class="dropdown-item" href="<?= base_url("/{$key}{$request_uri}"); ?>"><?= $value['title']; ?></a>
+                                    </li>
+                                <?php endif; ?>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            </div>
         </div>
     </div>
 </nav>
